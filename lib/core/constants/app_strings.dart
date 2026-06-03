8d978a92c65188aa20860719f8d5eabcb4441c29
MOHAMMAD.ALOTOL <muhammadalalotol@gmail.com>
2026-06-03 15:46:31 +0300

    Added application strings and core constants

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,17 @@
+class AppStrings {
+  static const String appTitle = 'مستودع التدريب الميداني';
+  static const String personalInfoTitle = 'المعلومات الشخصية';
+
+  static const String studentNameLabel = 'الاسم';
+  static const String studentNameValue = 'محمد عبدالله العطل';
+
+  static const String jobTitleLabel = 'المسمى الوظيفي';
+  static const String jobTitleValue = 'مبرمج قواعد بيانات ولغات متعددة';
+
+  static const String specialtyLabel = 'التخصص';
+  static const String specialtyValue = 'تطوير تطبيقات الهاتف المحمول (Flutter)';
+
+
+  static const String copyright = 'جميع الحقوق محفوظة © 2026';
+  static const String developedBy = 'تم التطوير بواسطة محمد العطل لتلبية متطلبات التدريب الميداني';
+}
